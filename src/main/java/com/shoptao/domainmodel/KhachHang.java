cff44f1381a8b251e8be4a21c95b9045d0ca2be9
nguyen293293 <phanvannguyen020903@gmail.com>
2022-11-16 22:28:57 +0700

    nguyen mapping entity sql

--- src/main/java/com/shoptao/domainmodel/KhachHang.java
+++ src/main/java/com/shoptao/domainmodel/KhachHang.java
@@ -1,9 +1,43 @@
 package com.shoptao.domainmodel;
 
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
 /**
  *
  * @author nguyen293
  */
-public class KhachHang {
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "KhachHang")
+public class KhachHang implements Serializable{
     
+    @Id
+    @GenericGenerator(name = "generator", strategy = "guid", parameters = {})
+    @GeneratedValue(generator = "generator")
+    private String id;
+    
+    private String ma;
+    
+    private String hoten;
+    
+    private Date ngaysinh;
+    
+    private String sdt;
+    
+    private String diachi;
+    
+    private int trangthai;
 }
--- src/main/java/com/shoptao/domainmodel/KhuyenMai.java
+++ src/main/java/com/shoptao/domainmodel/KhuyenMai.java
@@ -1,9 +1,43 @@
 package com.shoptao.domainmodel;
 
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
 /**
  *
  * @author nguyen293
  */
-public class KhuyenMai {
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "KhuyenMai")
+public class KhuyenMai implements Serializable {
     
+    @Id
+    @GenericGenerator(name = "generator", strategy = "guid", parameters = {})
+    @GeneratedValue(generator = "generator")
+    private String id;
+    
+    private String ma;
+    
+    private String ten;
+    
+    private double giatri;
+    
+    private Date ngaybatdau;
+    
+    private Date ngayketthuc;
+    
+    private int trangthai;
 }
