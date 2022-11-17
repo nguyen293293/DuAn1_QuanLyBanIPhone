15420c10f67363abe52a501cc9c39733b67517f7
2R <haih70872@gmail.com>
2022-11-17 21:54:51 +0700

    update service

--- src/main/java/com/shoptao/services/interfaces/ChungService.java
+++ src/main/java/com/shoptao/services/interfaces/ChungService.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package com.shoptao.services.interfaces;
 
@@ -11,7 +11,7 @@ import java.util.List;
  * @author haih7
  */
 public interface ChungService<T> {
-   List<T> getList();
+     List<T> getList();
    String add(T t);
     String update( T t);
     T getOne(String id);
